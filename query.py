f04b5371e9f511d088a3cbbfdd35f6cdc6cb4a57
Lin Huaian <linhuaian@Lins-MacBook-Air-3.local>
2023-09-11 17:01:39 +0800

    Removed dotenv

--- query.py
+++ query.py
@@ -1,11 +1,5 @@
-import requests
-import json
-from dotenv import load_dotenv
 import os
 import datetime as dt
-
-load_dotenv()
-MYSQL_CONNECTOR_URL = chat_id = os.environ.get("RESOURCE_URL")
 
 
 def query(dataframe):
