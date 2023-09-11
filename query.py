1237b1d2344d5740864ddfeeecaf61efdbc088fc
Lin Huaian <linhuaian@Lins-MacBook-Air-3.local>
2023-09-11 17:16:09 +0800

    fixed incorrect path name format

--- query.py
+++ query.py
@@ -7,7 +7,7 @@ def query(dataframe):
     dt = datetime.now().replace(tzinfo=timezone.utc)
     year = dt.strftime('%Y')
     week = dt.strftime("%V")
-    file_name = f"news/news_week_{week}_{year}.csv"
+    file_name = f"/news/news_week_{week}_{year}.csv"
     if os.path.exists(file_name):
         #dataframe.to_csv(file_name, mode='a', header=None, index=False)
         pd.read_csv(file_name).append(dataframe).drop_duplicates(subset=["week", "headline", "news_channel"],
